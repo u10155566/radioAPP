d8aeed0acee8f9be7297cee9f8debbba78b84ab4
U1065206 <u10155566@gmail.com>
2020-04-27 10:06:52 +0800

    補上程式碼

--- app/src/main/java/com/u1065206/radioapp/MainActivity.java
+++ app/src/main/java/com/u1065206/radioapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.u1065206.radioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        TextView txvLike=findViewById(R.id.txvLike);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+
+        int id = radioGroup.getCheckedRadioButtonId();
+        RadioButton seleted = findViewById(id);
+
+        txvLike.setText("最愛組合是:"+seleted.getText());
     }
 }
