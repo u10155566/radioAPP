3e8dbed6235ad763d92d84a958e608db2efd8d74
U1065206 <u10155566@gmail.com>
2020-04-27 10:11:25 +0800

    補上說明

--- app/src/main/java/com/u1065206/radioapp/MainActivity.java
+++ app/src/main/java/com/u1065206/radioapp/MainActivity.java
@@ -16,12 +16,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnOK(View view) {
+        //設定各屬性的值
         TextView txvLike=findViewById(R.id.txvLike);
         RadioGroup radioGroup = findViewById(R.id.radioGroup);
 
+        //讓seleted取得選中選項的值
         int id = radioGroup.getCheckedRadioButtonId();
         RadioButton seleted = findViewById(id);
 
+        //讓答案顯示出選中選項的內容
         txvLike.setText("最愛組合是:"+seleted.getText());
     }
 }
